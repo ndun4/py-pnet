232f71065b28904fbcdcfcb8a40cdeeda6c11057
Neal Duncan <nealcduncan@gmail.com>
2016-08-11 15:59:44 -0400

    removed  APIConfig class

--- py_pnet.py
+++ py_pnet.py
@@ -1,17 +1,5 @@
 import requests
 
-class APIConfig(object):
-	"""Defines class to hold static configuration variables"""
-
-
-	# The only thing you should change in this class is the values in the key dict.
-	url = 'https://api.phish.net/'
-	endpoint = {'json': 'api.js', 'php': 'api.php'}
-	key = {'public': 'pubkey', 'api': 'yourapikey'}
-	api_ver = {'2.0': '2.0', '1.0': '1.0'}
-	format = {'json': 'json', 'php': 'php'}
-
-
 
 
 class PnetClient(object):
@@ -21,20 +9,20 @@ class PnetClient(object):
 	# modify this library by adding additional presets in dict. format.
 	# However, this lib is built on the notion that you'd have a dict of key-value pairs pulling the values
 	# in from ::class::APIConfig, so that would be the most straightforward way to change your config presets
-	defaults = {'api': APIConfig.api_ver['2.0'], 'format': APIConfig.format['json'], 'apikey': APIConfig.key['api']} 
+	 
 
 
 	# You can replace the value for ::kwarg::endpoint with 'php' for an impromptu request for a php object
-	def __init__(self, endpoint='json', config=defaults):
+	def __init__(self, apikey=None):
 		super(PnetClient, self).__init__()
+		self.apikey = apikey
+		self.config = {'api': '2.0', 'format': 'json', 'apikey': self.apikey}
 		
-		self.config = config
-		self.endpoint = APIConfig.endpoint[endpoint]
 		
 	# GET method API Call
 	def get(self, query_method, query_filter=None, query_filter_value=None, callback=None):
 
-		target = APIConfig.url + self.endpoint
+		target = 'https://api.phish.net/api.js'
 		params = self.config
 		params.update({'method': query_method, query_filter: query_filter_value})
 		
@@ -44,7 +32,7 @@ class PnetClient(object):
 	# POST method API Call
 	def post(self, query_method, query_filter=None, query_filter_value=None, callback=None):
 
-		target = APIConfig.url + self.endpoint
+		target = 'https://api.phish.net/api.js'
 		params = self.config
 		params.update({'method': query_method, query_filter: query_filter_value})
 		
